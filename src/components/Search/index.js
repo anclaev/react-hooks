65461a0b1df17769e5a2eadd9bfd170d776eae5e
anclaev <iahugo@yandex.ru>
2021-04-06 16:01:29 +0400

    Валидация поля поиска.

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,19 +1,30 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AlertContext } from "../../context/Alert";
 
 export const Search = () => {
+  const [val, setVal] = useState("");
+
   const { show } = useContext(AlertContext);
 
   const onSubmit = (e) => {
-    if (e.key === "Enter") show("This is alert!");
+    if (e.key !== "Enter") return;
+
+    if (val.trim()) {
+      console.log("Make request with: ", val);
+    } else {
+      show("Введите корректный ник.");
+    }
   };
+
   return (
     <div className="form-group">
       <input
         type="text"
         className="form-control"
         placeholder="Введите ник пользователя..."
+        value={val}
         onKeyPress={onSubmit}
+        onChange={(e) => setVal(e.target.value)}
       />
     </div>
   );
