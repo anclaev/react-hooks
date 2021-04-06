5de47879a947c3b54c99a3100862458775d2770c
anclaev <iahugo@yandex.ru>
2021-04-06 15:12:04 +0400

    Страница About.

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,10 @@
 export const About = () => (
-  <div>
-    <h1>Информация</h1>
+  <div className="jumbotron jumbotron-fluid">
+    <div className="container">
+      <h1 className="display-4">О нас</h1>
+      <p className="">
+        Версия приложения: <strong>1.0.0</strong>
+      </p>
+    </div>
   </div>
 );
