ba10c6c07960edd8586f81504d77a254f90355fa
anclaev <iahugo@yandex.ru>
2021-04-06 15:05:17 +0400

    Организация роутинга.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
+import { About } from "./pages/About";
 import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
       <div className="container pt-4">
-        <Home />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/profile/:name" component={Profile} />
+        </Switch>
       </div>
-    </>
+    </BrowserRouter>
   );
 };
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,16 +1,18 @@
+import { NavLink } from "react-router-dom";
+
 export const Navbar = () => (
   <nav className="navbar navbar-dark bg-primary navbar-expand-lg">
     <div className="navbar-brand">GitHub Поиск</div>
     <ul className="navbar-nav">
       <li className="nav-item">
-        <a href="/" className="nav-link">
+        <NavLink exact to="/" className="nav-link">
           Главная
-        </a>
+        </NavLink>
       </li>
       <li className="nav-item">
-        <a href="/about" className="nav-link">
+        <NavLink to="/about" className="nav-link">
           Информация
-        </a>
+        </NavLink>
       </li>
     </ul>
   </nav>
